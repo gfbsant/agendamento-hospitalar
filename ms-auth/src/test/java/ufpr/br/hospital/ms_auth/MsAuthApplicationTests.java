ba4004e1a3614d36e4f0147829529d7e2a198010
gfbsant <gfbsant@gmail.com>
2025-05-15 08:09:31 -0300

    Refactor configuration to use environment variables and add .gitignore

--- ms-auth/src/test/java/ufpr/br/hospital/ms_auth/MsAuthApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package ufpr.br.hospital.ms_auth;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MsAuthApplicationTests {
-
-}
